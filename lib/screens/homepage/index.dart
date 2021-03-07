704010030de56ff7e36dea0d21cc469037a87ab9
Alice Li <alice.li@uptake.com>
2021-03-06 19:27:52 -0800

    get and display google profile photos

--- lib/screens/homepage/index.dart
+++ lib/screens/homepage/index.dart
@@ -1,10 +1,29 @@
 // https://github.com/AgoraIO-Community/Agora-Flutter-Quickstart
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:letsmuk/shared/avatar_image.dart';
 import 'package:letsmuk/services/agora.dart';
 
+Future<http.Response>  fetchFriendPic(String uid) async{
+  return await http.get(Uri.https('content-people.googleapis.com', '/v1/people/' + uid,
+    { "personFields" : "photos", "sources": "READ_SOURCE_TYPE_PROFILE", "key": "AIzaSyAMoW3IMJ6qnc1ZRSswEZMryM8TdvJMbNI" }));
+}
+
+var happeningNow = [{
+    "name": "jeff",
+    "uid": "112150203641622258272",
+  },{
+    "name": "Alice",
+    "uid": "111376690246824336476",
+  },
+  {
+    "name": "Hyunzee",
+    "uid": "110594606043658856673",
+  }];
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.user}) : super(key: key);
@@ -29,12 +48,29 @@ class HomePage extends StatefulWidget {
     "start": "12:00",
     "end": "12:40",
   }];
+  
+  
+
 
   @override
   State<StatefulWidget> createState() => IndexState();
 }
 
 class IndexState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  } 
+  void initialize() async {
+    print('hello');
+        for(var item in happeningNow) {
+
+         final response = await fetchFriendPic(item['uid']);
+         Map<String, dynamic> responseJson = json.decode(response.body);
+         item['photoURL'] = responseJson['photos'][0]['url'];
+  }}
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -90,12 +126,54 @@ class IndexState extends State<HomePage> {
               GestureDetector(
                 child: Column(
                   children: <Widget>[
-                    Text(
-                      'Who are you eating lunch with today?',
+                    Container(
+                      child: Text(
+                      'Hop into a lunch room happening now',
                       textAlign: TextAlign.left,
                       overflow: TextOverflow.clip,
                       softWrap: true,
-                      style: TextStyle(fontSize: 18),
+                      style: TextStyle(fontSize: 14),
+                    ),
+                      ),
+                    for(var item in happeningNow ) Container(
+                      padding: const EdgeInsets.symmetric(vertical: 1),
+                      child: Chip(
+                        backgroundColor: Color.fromRGBO(254,232,166,1),
+                        avatar: CircleAvatar(
+                          backgroundColor: Colors.white,
+                          
+                          child: Container(
+                          width: 200,
+                          height: 200,
+                          decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: NetworkImage(item['photoURL']),
+                            fit: BoxFit.fill
+                          ),
+                          ),
+                        ),
+                        ),
+                        label: Text("Join ${item['name']}'s lunch room"),
+                      ))
+                  ],
+                ),
+                onTap: () {
+                  widget._agora.joinRoom(context);
+                },
+              ),
+              GestureDetector(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.fromLTRB(0, 30, 0, 10),
+                      child: Text(
+                        'Who are you eating lunch with today?',
+                        textAlign: TextAlign.left,
+                        overflow: TextOverflow.clip,
+                        softWrap: true,
+                        style: TextStyle(fontSize: 18),
+                      ),
                     ),
                     for(var item in widget.friends ) Container(
                       padding: const EdgeInsets.symmetric(vertical: 10),
