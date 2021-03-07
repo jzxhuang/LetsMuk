f4e31cced3fb6e43a45f2bd63c1a31962ccdaad7
Alice Li <alice.li@uptake.com>
2021-03-06 21:07:19 -0800

    change font

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'theme.dart';
 
 // Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
@@ -27,22 +28,23 @@ class MyApp extends StatelessWidget {
         value: AuthService().user,
         child: MaterialApp(
           title: 'Flutter Demo',
-          theme: ThemeData(
-            // This is the theme of your application.
-            //
-            // Try running your application with "flutter run". You'll see the
-            // application has a blue toolbar. Then, without quitting the app, try
-            // changing the primarySwatch below to Colors.green and then invoke
-            // "hot reload" (press "r" in the console where you ran "flutter run",
-            // or simply save your changes to "hot reload" in a Flutter IDE).
-            // Notice that the counter didn't reset back to zero; the application
-            // is not restarted.
-            primarySwatch: Colors.blue,
-            // This makes the visual density adapt to the platform that you run
-            // the app on. For desktop platforms, the controls will be smaller and
-            // closer together (more dense) than on mobile platforms.
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-          ),
+          theme: ApplicationTheme(context),
+          // theme: ThemeData(
+          //   // This is the theme of your application.
+          //   //
+          //   // Try running your application with "flutter run". You'll see the
+          //   // application has a blue toolbar. Then, without quitting the app, try
+          //   // changing the primarySwatch below to Colors.green and then invoke
+          //   // "hot reload" (press "r" in the console where you ran "flutter run",
+          //   // or simply save your changes to "hot reload" in a Flutter IDE).
+          //   // Notice that the counter didn't reset back to zero; the application
+          //   // is not restarted.
+          //   primarySwatch: Colors.blue,
+          //   // This makes the visual density adapt to the platform that you run
+          //   // the app on. For desktop platforms, the controls will be smaller and
+          //   // closer together (more dense) than on mobile platforms.
+          //   visualDensity: VisualDensity.adaptivePlatformDensity,
+          // ),
           home: Wrapper(),
         ));
   }
